d8f89a267ff486fabdc4bf74ef110bbb917e80e4
17sathya <sathyakumar17c@gmail.com>
2026-01-19 19:46:25 +0530

    Day 2 :MongoDB connection and core database schema

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-const express=require('express');
-const app=express();
-const cors=require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('Server is running');
-}
+// connect database
+connectDB();
 
-);
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
 
-const PORT=process.env.PORT||5000;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
